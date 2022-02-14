d89354230a284fc73855d8a128a1eb59a7580abf
mateusz <mateusz.jeziorowski@gmail.com>
2022-02-14 22:27:56 +0100

    Create controller class
    -created controller class template

---
+++ controller.py
@@ -0,0 +1,21 @@
+from app import Camera
+from threading import Thread
+
+class Controler:
+    def __init__(self) -> None:
+        pass
+
+    def loop_recording(self):
+        pass
+
+    def normal_recording(self):
+        pass
+
+    def motion_recording(self):
+        pass
+
+    def terminate_recording(self):
+        pass
+
+    def restart_thread():
+        pass
